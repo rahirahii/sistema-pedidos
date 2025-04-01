d5142595901ba6b4d97d79dc7f15e9a8d55a5a42
rahirahii <rahiiany.viclarry@gmail.com>
2025-04-01 10:50:35 -0300

    login

---
+++ app/Livewire/Auth/Login/Cliente.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Auth\Login;
+
+use Livewire\Component;
+
+class Cliente extends Component
+{
+    public function render()
+    {
+        return view('livewire.auth.login.cliente');
+    }
+}
