f138e8f5fb3939489d8e55dbd27a1f39214dc048
rahirahii <rahiiany.viclarry@gmail.com>
2025-04-01 10:18:54 -0300

    layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire\Cliente\Create;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/cadastro/cliente', Create::class);
